5803a2fa54a03b6cb39c4190788235646024e0d2
wejdangamal <wg63wegdan92@gmail.com>
2023-12-20 03:06:39 +0200

    Isolate Booking Service "(HINT) this commit after Pushing the project on Time as Required and now I do code Refactoring"

--- Vzeeta.Services/Services/BookingServices/BookingService.cs
+++ Vzeeta.Services/Services/BookingServices/BookingService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vzeeta.Core.Model;
+﻿using Vzeeta.Core.Model;
 using Vzeeta.Core.Model.Enums;
 using Vzeeta.Services.Interfaces;
 
@@ -13,19 +8,22 @@ namespace Vzeeta.Services.Services.BookingServices
     {
         public void calculateFinalPrice(DiscountCode_Coupon code, int userRequestsCount, ref decimal Price)
         {
-                if (userRequestsCount % code.NoOfRequests == 0)
+            if (userRequestsCount % code.NoOfRequests == 0)
+            {
+                if (code.DiscountType == DiscountType.Percentage)
                 {
-                    if (code.DiscountType == DiscountType.Percentage)
-                    {
-                        Price = Price < code.Value ? 0 : Price - Price * (code.Value / 100);
-                    }
-                    else
-                    {
-                        Price = Price < code.Value ? 0 : Price - code.Value;
-                    }
-
+                    Price = Price < code.Value ? 0 : Price - Price * (code.Value / 100);
                 }
+                else
+                {
+                    Price = Price < code.Value ? 0 : Price - code.Value;
+                }
+
+            }
+            else
+            {
                 throw new Exception($"Invalid Number Of Completed Requests to use code");
+            }
         }
     }
 }
